9778a888685e7b8f032f036a8e3427a38544f11b
Paul Rancuret <pmrancuret@gmail.com>
2016-09-18 09:22:17 -0400

    Add LCD library to project.  Rearrange source and include files into src/ and inc/ directories.  Add initialization of LCD screen.

--- Code/src/fanControl.cpp
+++ Code/src/fanControl.cpp
@@ -1,15 +1,21 @@
 // Do not remove the include below
 #include "FanControl.h"
+#include "lcd.h"
 
 
 // The setup function is called once at startup of the sketch
 void setup ( )
 {
 // Add your initialization code here
+  lcd_init ( LCD_DISP_ON );         // initialize and turn on LCD display
+  lcd_clrscr ( );                   // clear screen and cursor on LCD
+  lcd_puts ( "INITIALIZING...\n" ); // set screen to say initializing
+
 }
 
 // The loop function is called in an endless loop
 void loop ( )
 {
 // Add your repeated code here
+
 }
