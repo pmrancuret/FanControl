b80e7e162d091eee6f43ca5969b77245ecd4bde2
Paul Rancuret <pmrancuret@gmail.com>
2016-11-05 11:23:53 -0400

    Adjust LCD screen alignment in button debug mode.

--- Code/src/fanCtrlStateMachine.cpp
+++ Code/src/fanCtrlStateMachine.cpp
@@ -453,13 +453,13 @@ static FANCTRLSTATE_ENUM_TYPE debugBtnState ( FANCTRLSTATE_ENUM_TYPE thisState )
     lcdLoops = 0; // reset LCD loop counter
 
     /* First line has button rising edge counts */
-    sprintf ( lcdBuff, "%4u %4u %4u", btn1EdgCnt, btn2EdgCnt, btn3EdgCnt ); // set button info
-    lcd.setCursor ( 0, 0 );                                                 // set cursor to start of first line on LCD
-    lcd.print ( lcdBuff );                                                  // print first line
+    sprintf ( lcdBuff, "  %4u %4u %4u", btn1EdgCnt, btn2EdgCnt, btn3EdgCnt ); // set button info
+    lcd.setCursor ( 0, 0 );                                                   // set cursor to start of first line on LCD
+    lcd.print ( lcdBuff );                                                    // print first line
 
     /* Second line has button consecutive counts */
-    sprintf ( lcdBuff, "%4u %4u %4u", btn1PressCnt, btn2PressCnt, btn3PressCnt ); // set button info
-    lcd.setCursor ( 0, 1 );                                                       // set cursor to start of second line on LCD
+    sprintf ( lcdBuff, "%  4u %4u %4u", btn1PressCnt, btn2PressCnt, btn3PressCnt ); // set button info
+    lcd.setCursor ( 0, 1 );                                                         // set cursor to start of second line on LCD
     lcd.print ( lcdBuff );
 
   }
